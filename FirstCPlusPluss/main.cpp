fa4b03224299e3e91080dfcfef10370ddd921552
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-01-23 08:34:40 -0700

    Initial commit

---
+++ FirstCPlusPluss/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FirstCPlusPluss
+//
+//  Created by Segota, Sheradon on 1/23/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    return 0;
+}
